c6716c298afb67d2df0d3d8a83b7128cd5552ebd
rutap <49236059+rutap@users.noreply.github.com>
2019-05-06 13:33:48 +0200

    Over 20 button has a function now. close #3

--- app/src/main/java/se/juneday/systemetappbasic/MainActivity.java
+++ app/src/main/java/se/juneday/systemetappbasic/MainActivity.java
@@ -1,5 +1,49 @@
 package se.juneday.systemetappbasic;
 
-public class MainActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button Button2;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button2 = (Button) findViewById(R.id.button2);
+
+    }
+
+    public void onStart() {
+        super.onStart();
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton(){
+        Button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (((Button) v).isClickable()) {
+                    setContentView(R.layout.second_activity);
+                    Intent i = new Intent(MainActivity.this, SecondActivity.class);
+                    startActivity(i);
+                }
+            }
+        });
+
+
+
+
+
+    }
+
+
+
+
 
 }
