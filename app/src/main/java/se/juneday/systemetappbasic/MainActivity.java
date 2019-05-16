bb4040e5cfddf3bd3eae5425bbb6bcbec94069f4
rutap <ruta.pedece@gmail.com>
2019-05-16 18:30:20 +0200

    Half done. Seems like it is working when language is changed on the Main Activity

--- app/src/main/java/se/juneday/systemetappbasic/MainActivity.java
+++ app/src/main/java/se/juneday/systemetappbasic/MainActivity.java
@@ -1,12 +1,20 @@
 package se.juneday.systemetappbasic;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.Toast;
+
+import java.util.Locale;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class MainActivity extends AppCompatActivity {
     private Button Button2;
@@ -14,9 +22,17 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonsubmit;
     private RatingBar rating_b;
 
+    //added
+    // these two variables will be used by SharedPreferences
+    private static final String FILE_NAME = "file_lang"; // preference file name
+    private static final String KEY_LANG = "key_lang"; // preference key
+    // added
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // please load language after super and before setContentView
+        loadLanguage();
         setContentView(R.layout.activity_main);
         onButtonClickListener();
 
@@ -25,7 +41,62 @@ public class MainActivity extends AppCompatActivity {
         button1 = (Button) findViewById(R.id.button1);
         rating_b =(RatingBar) findViewById(R.id.ratingbar);
 
+
+
     }
+
+    private void saveLanguage(String lang) {
+
+
+        // we can use this method to save language
+        SharedPreferences preferences = getSharedPreferences(FILE_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(KEY_LANG, lang);
+        editor.apply();
+        // we have saved
+        // recreate activity after saving to load the new language, this is the same
+        // as refreshing activity to load new language
+
+        recreate();
+
+    }
+    private void loadLanguage() {
+        // we can use this method to load language,
+        // this method should be called before setContentView() method of the onCreate method
+
+        Locale locale = new Locale(getLangCode());
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getResources().updateConfiguration(config, getResources().getDisplayMetrics());
+    }
+    private String getLangCode() {
+        SharedPreferences preferences = getSharedPreferences(FILE_NAME, MODE_PRIVATE);
+        String langCode = preferences.getString(KEY_LANG, "en");
+        // save english 'en' as the default language
+        return langCode;
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.language_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_english:
+                saveLanguage("en");
+                break;
+
+            case R.id.action_swedish:
+                saveLanguage("sv");
+                break;
+
+
+        }
+        return true;
+    }
+
 
     public void onResume() {
         super.onResume();
